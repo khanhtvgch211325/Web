936229dbd9667db2c644e6c9a71f32b0522399e7
anhdat1121 <nguyenminhdat21022003@gmail.com>
2023-08-23 04:40:41 +0700

    UI for login form

--- FurnitureInventory/resources/views/login.blade.php
+++ FurnitureInventory/resources/views/login.blade.php
@@ -2,12 +2,12 @@
 @extends('layouts.app')
 @section('title','Login Form')
 @section('main')
-<form action="/login" method="post" class="container" >
+<!-- <form action="/login" method="post" class="container" >
     @csrf
-    <!-- <input type="text" name="name" placeholder="Username">
+    <input type="text" name="name" placeholder="Username">
     <input type="password" name="password" placeholder="Password">
     <input type="submit" value="Login"> -->
-    <div class="container">
+    <!-- <div class="container">
       <label for="name"><b>Username</b></label>
       <input type="text" placeholder="Enter Username" name="name" required>
 
@@ -17,5 +17,37 @@
       <button type="submit" >Login</button>
      
     </div>
-</form>
+</form> --> 
+<form action="/login" method="post" class="container" >
+<div id="login"><!--I take form from: https://bootsnipp.com/snippets/bxzmb-->
+  @csrf     
+  <h3 class="text-center text-white pt-5">Login form</h3>
+        <div class="container">
+            <div id="login-row" class="row justify-content-center align-items-center">
+                <div id="login-column" class="col-md-6">
+                    <div id="login-box" class="col-md-12">
+                        <form id="login-form" class="form" action="" method="post">
+                            <h3 class="text-center text-info">Login</h3>
+                            <div class="form-group">
+                                <label for="name" class="text-info">Username:</label><br>
+                                <input type="text" name="name" id="name" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="password" class="text-info">Password:</label><br>
+                                <input type="text" name="password" id="password" class="form-control">
+                            </div>
+                            <div class="form-group">
+                              
+                                <input type="submit" name="submit" class="btn btn-info btn-md" value="Submit">
+                            </div>
+                            <div id="register-link" class="text-right" style="margin: -35px 0 0 500px;">
+                                <a href="/register" class="text-info btn btn-primary">Register here</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </form>
 @endsection
